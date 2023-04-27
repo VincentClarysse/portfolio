5a52d13b00f63b72b9cf6bbd589f2e769dfe0911
Vincent Clarysse <claryssevincent@gmail.com>
2023-04-27 17:30:32 +0200

    fix scrolling bug

--- script/sript.js
+++ script/sript.js
@@ -4,22 +4,17 @@ let projectsection = document.querySelector(".projectsection");
 let projectdivs = document.getElementsByClassName("projectdiv");
 let lilnavs = document.getElementsByClassName("lil-nav-link");
 
-let image1_height = document.getElementById("image-1").clientHeight;
-gallery.style.height= image1_height + "px";
-lilnav.style.height= image1_height + "px";
-projectsection.style.height= image1_height + "px";
-for(i=0 ; i<projectdivs.length; i++){
-    projectdivs[i].style.height= image1_height +"px";
-}
+window.onload = (event) => {
+    let image1_height = document.getElementById("image-1").offsetHeight;
+    gallery.style.height= image1_height + "px";
+    lilnav.style.height= image1_height + "px";
+    for(i=0 ; i<projectdivs.length; i++){
+        projectdivs[i].style.height= image1_height +"px";
+    }
+    projectsection.style.height= image1_height + "px";
 
-console.log(projectdivs[0].offsetTop);
-console.log(projectdivs[1].offsetTop);
-console.log(projectdivs[2].offsetTop);
-
-projectdivs[2].scrollTo({
-    top: 0,
-    behavior: "smooth",
-  });
+    console.log(image1_height);
+};
 
 window.addEventListener("resize", (event) => {
     let image1_height = document.getElementById("image-1").clientHeight;
@@ -29,17 +24,20 @@ window.addEventListener("resize", (event) => {
         projectdivs[i].style.height= image1_height +"px";
     }
     projectsection.style.height= image1_height + "px";
+
+    let galleryarray = document.getElementsByClassName("gallery__img");
+    for(i=0 ; i<galleryarray.length; i++){
+        galleryarray[i].style.maxHeight= "none";
+    }
 });
 
 for(let i=0; i<lilnavs.length; i++) {
     lilnavs[i].addEventListener("click",()=>{
         let image1_height = document.getElementById("image-1").clientHeight;
+        topheight = image1_height * i
         projectsection.scrollTo({
-            top: image1_height * i,
+            top: topheight,
             behavior: "smooth",
           });
     })
 }
-
-
-
