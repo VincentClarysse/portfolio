3dd0b738d2ed809f4a352af3aa79be5e5ddf3609
Vincent Clarysse <claryssevincent@gmail.com>
2023-05-02 00:05:32 +0200

    intersect api basics

--- script/sript.js
+++ script/sript.js
@@ -3,6 +3,17 @@ let lilnav = document.querySelector(".lil-nav");
 let projectsection = document.querySelector(".projectsection");
 let projectdivs = document.getElementsByClassName("projectdiv");
 let lilnavs = document.getElementsByClassName("lil-nav-link");
+const section = document.getElementById("section");
+const grad_hr = document.getElementById("grad_hr");
+
+const carrousel_section = document.getElementById('carrousel_main')
+
+let left = (section.offsetLeft);
+let width = (section.clientWidth);
+
+
+
+
 
 window.onload = (event) => {
     let image1_height = document.getElementById("image-1").offsetHeight;
@@ -13,7 +24,15 @@ window.onload = (event) => {
     }
     projectsection.style.height= image1_height + "px";
 
-    console.log(image1_height);
+    grad_hr.style.background= "linear-gradient(to right, " 
+    + "black "+ (left-11)+"px"
+    + ", " 
+    + "transparent "+ (left-10)+"px"
+    + ", " 
+    + "transparent "+ (left+width+7)+"px"
+    + ", " 
+    + "black "+ (left+width+8)+"px"
+    + ")";
 };
 
 window.addEventListener("resize", (event) => {
@@ -22,22 +41,68 @@ window.addEventListener("resize", (event) => {
     lilnav.style.height= image1_height + "px";
     for(i=0 ; i<projectdivs.length; i++){
         projectdivs[i].style.height= image1_height +"px";
+
     }
     projectsection.style.height= image1_height + "px";
 
     let galleryarray = document.getElementsByClassName("gallery__img");
     for(i=0 ; i<galleryarray.length; i++){
         galleryarray[i].style.maxHeight= "none";
+        // galleryarray[i].style.maxHeight= galleryarray[i].style.height;
     }
+
+    let currentleft = (section.offsetLeft);
+    let currentwidth = (section.clientWidth);
+
+    grad_hr.style.background= "linear-gradient(to right, " 
+    + "black "+ (currentleft-11)+"px"
+    + ", " 
+    + "transparent "+ (currentleft-10)+"px"
+    + ", " 
+    + "transparent "+ (currentleft+currentwidth+7)+"px"
+    + ", " 
+    + "black "+ (currentleft+currentwidth+8)+"px"
+    + ")";
 });
 
 for(let i=0; i<lilnavs.length; i++) {
     lilnavs[i].addEventListener("click",()=>{
         let image1_height = document.getElementById("image-1").clientHeight;
-        topheight = image1_height * i
+        topheight = image1_height * i ;
         projectsection.scrollTo({
             top: topheight,
             behavior: "smooth",
           });
+
+        gallery_topheight = 
+        gallery.scrollTo({
+        top: topheight + (10*i),
+        behavior: "smooth",
+        });
     })
+}
+
+
+
+const observer = new IntersectionObserver(function (entries, observer) 
+{
+    for(let i =0; i < entries.length ; i++) {
+        if(entries[i].isIntersecting){
+            console.log(entries[i].target)
+            }
+    }
+    }
+    ,{
+        threshold: 0.05
+    });
+
+const the_animation = document.querySelectorAll('.main');
+
+for (let i = 0; i < the_animation.length; i++) {
+    const elements = the_animation[i];
+
+setTimeout(()=> {
+    observer.observe(elements);
+},1000)
+
 }
